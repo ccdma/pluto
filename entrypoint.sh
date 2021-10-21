6e736be75068a4e0889a48b3669927e0ff1f0bd7
face0u0 <me@face0u0.net>
2021-10-21 14:06:35 +0900

    push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# create user
+USERHOME=/home/user
+USERNAME=user
+echo "Starting with UID : ${USER_ID:=1001}, GID: ${GROUP_ID:=1001}"
+groupadd ${USERNAME} -g ${GROUP_ID} && \
+useradd -u ${USER_ID} -g ${GROUP_ID} -d ${USERHOME} -m -s /bin/bash -G sudo ${USERNAME}
+
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
+# sudo時にPATHを引き継ぐ
+sed -i '/Defaults.*secure_path.*/d' /etc/sudoers && \
+echo 'Defaults  env_keep += "PATH"' >> /etc/sudoers
 
 if [ "$1" != "" ]; then
-    exec "$@"
+    exec /usr/sbin/gosu ${USERNAME} "$@"
+else
+    exec /usr/sbin/gosu ${USERNAME} bash
 fi
-
-sh
