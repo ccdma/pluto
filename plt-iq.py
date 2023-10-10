8f220b975dab77fb3b1c4396873e3cacdb1e305f
mu2chn <app@mu2chn.net>
2023-10-10 15:00:07 +0900

    update

--- plt-iq.py
+++ plt-iq.py
@@ -6,7 +6,14 @@ FILENAME = "a.csv"
 dump = np.loadtxt(FILENAME, delimiter=',')
 data = dump[0] + dump[1]*1j
 
-data = data[1000:1100]
-plt.scatter(data.real, data.imag, s=0.4)
-plt.plot(data.real, data.imag, lw=0.2)
-plt.savefig("1.png")
+data = data[1000:1050]
+
+# 円周上に正規化
+data = data / np.abs(data)
+
+fig, ax = plt.subplots()
+ax.scatter(data.real, data.imag, s=0.4)
+ax.plot(data.real, data.imag, lw=0.2)
+ax.set_aspect('equal')
+fig.tight_layout()
+fig.savefig("1.png")
