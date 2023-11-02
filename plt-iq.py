1a51cb7b97f52472754b633e00c79ecda30b586c
mu2chn <app@mu2chn.net>
2023-11-02 13:58:50 +0900

    update

--- plt-iq.py
+++ plt-iq.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from madi import code
+from madi.static import *
 
 def args_mul(arr: np.ndarray, n: int):
     arg1 = np.angle(arr[:-1])*n
@@ -13,7 +14,12 @@ OUT_FILE = f"{IN_FILE}.png"
 
 dump = np.loadtxt(IN_FILE, delimiter=',')
 data = dump[0] + dump[1]*1j
-# data = np.tile(code.primitive_root_code(1019, 2, 1), 20)  # reference
+
+## reference code print
+# data = code.primitive_root_code(1019, 2, 1)  # reference
+# data = np.sin(np.linspace(0, 2*np.pi, 1024)) + 1j*np.cos(np.linspace(0, 2*np.pi, 1024))
+# data = np.tile(data, 20); print("warning: use reference code")
+
 data = data[1000:10000]
 # 円周上に正規化
 # data = data / np.abs(data)
@@ -41,8 +47,8 @@ for i, ax in enumerate(axes):
     pdata = data[start:start+size]
     ax.scatter(pdata.real, pdata.imag, s=0.4)
     ax.plot(pdata.real, pdata.imag, lw=0.2)
-    ax.set_title(f"{start}-")
+    ax.set_title(f"{start}-{start+size}")
     ax.set_aspect('equal')
-
+fig.suptitle(f"IQ plot of sine wave \n(T=1024, SAMPLE_RATE=5M, LO=923MHz,BANDWIDTH=10Mz)")
 fig.tight_layout()
 fig.savefig(OUT_FILE)
