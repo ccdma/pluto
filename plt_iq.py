9193d7dd95d30b44ac459337eb04fdce09d8f71b
mu2chn <app@mu2chn.net>
2023-11-13 14:38:09 +0900

    update

--- plt_iq.py
+++ plt_iq.py
@@ -3,10 +3,10 @@ import numpy as np
 from madi import code, algo
 from madi.static import *
 
-T = 64
+T = 53
 
 IN_FILE = f"t{T}-sin.csv"
-# IN_FILE = f"t{T}-pri.csv"
+IN_FILE = f"t{T}-pri.csv"
 # IN_FILE = f"a.csv"
 
 OUT_FILE = f"{IN_FILE}.png"
@@ -25,7 +25,7 @@ data = dump[0] + dump[1]*1j
 # data = np.sin(np.linspace(0, 2*np.pi, 1024)) + 1j*np.cos(np.linspace(0, 2*np.pi, 1024))
 # data = np.tile(data, 20); print("warning: use reference code")
 
-data = data[1000:10000]
+data = data[1000:11000]
 # 円周上に正規化
 # data = data / np.abs(data)
 
@@ -37,37 +37,37 @@ def args_mul(arr: np.ndarray, n: int):
     arg1 = np.angle(arr[:-1])*n
     arg2 = np.angle(arr[1:])
     divarg = algo.cmod(arg2 - arg1)
-    return np.mean(np.abs(divarg))
-
-# fig, ax = plt.subplots()
-# argplt = []
-# for arr in np.split(data, 100):
-#     argplt.append(args_mul(arr, n=2))
-# ax.plot(argplt, marker='o', markersize=5)
-# ax.set_title("argument difference for x_1 - x_0*2 [rad]")
-# ax.set_xlabel("chunck")
-# ax.set_ylabel("argument [rad]")
-# figconf(fig)
-# fig.savefig(OUT_FILE)
-
+    return np.mean(divarg)
 
 fig, ax = plt.subplots()
-pdata = algo.cmod(np.angle(data)[1:] - np.angle(data)[:-1])[:8000]
-ax.plot(pdata)
-ax.grid(True, axis='y')
-ax.set_xlabel("time [1/sample_rate]")
+argplt = []
+for arr in np.split(data, 100):
+    argplt.append(args_mul(arr, n=2))
+ax.plot(argplt, marker='o', markersize=5)
+ax.set_title("argument difference for x_1 - x_0*2 [rad]")
+ax.set_xlabel("chunck")
 ax.set_ylabel("argument [rad]")
-fig.suptitle(f"""
-Argument difference of sine wave
-(t={T}, {RF_PARAM_DESC})
-""".strip())
 figconf(fig)
 fig.savefig(OUT_FILE)
-average = np.mean(pdata)
-expected = np.pi*2/T
-print(f"average={average}[rad]")
-print(f"expected={expected}[rad]")
-print(- average - expected)
+
+
+# fig, ax = plt.subplots()
+# pdata = algo.cmod(np.angle(data)[1:] - np.angle(data)[:-1])[:8000]
+# ax.plot(pdata)
+# ax.grid(True, axis='y')
+# ax.set_xlabel("time [1/sample_rate]")
+# ax.set_ylabel("code[t+1] - code[t] [rad]")
+# fig.suptitle(f"""
+# Argument difference of sine wave
+# (t={T}, {RF_PARAM_DESC})
+# """.strip())
+# figconf(fig)
+# fig.savefig(OUT_FILE)
+# average = np.mean(pdata)
+# expected = np.pi*2/T
+# print(f"average={average}[rad]")
+# print(f"expected={expected}[rad]")
+# print(- average - expected)
 
 
 fig, axes = plt.subplots(nrows=2, ncols=2, squeeze=False)
@@ -97,4 +97,4 @@ IQ plot of sine wave
 (t={T}, {RF_PARAM_DESC})
 """.strip())
 figconf(fig)
-fig.savefig(OUT_FILE)
+# fig.savefig(OUT_FILE)
